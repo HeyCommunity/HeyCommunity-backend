664d8538fb83efa0a35ec9928dca11a98b587205
SupeRodv2 <rod@protobia.tech>
2021-04-02 21:57:45 +0800

    restore users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,24 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->integer('is_admin')->default(0)->comment('Is Admin');
+            $table->string('wx_open_id')->nullable()->comment('Wechat Open ID');
+            $table->string('wx_union_id')->nullable()->comment('Wechat Union ID');
+
+            $table->string('nickname')->comment('Nickname');
+            $table->string('realname')->nullable()->comment('Realname');
+            $table->string('gender')->nullable()->comment('Gender');
+            $table->string('bio')->nullable()->comment('Bio');
+            $table->string('avatar')->default('images/users/avatar/default.jpg')->comment('Avatar');
+            $table->string('profile_bg_img')->default('images/users/profile_bg_imgs/default.jpg')->comment('Avatar');
+
+            $table->string('phone')->nullable()->comment('Phone');
+            $table->string('email')->nullable()->comment('Email');
+            $table->string('password')->nullable()->comment('Password');
+
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
