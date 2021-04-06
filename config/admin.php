fd249aa56f4e3e337e3f85204b8e1d907d7feaad
SupeRodv2 <rod@protobia.tech>
2021-04-07 01:57:34 +0800

    configs

--- config/admin.php
+++ config/admin.php
@@ -22,7 +22,7 @@ return [
     | `img` tag, eg '<img src="http://logo-url" alt="Admin logo">'.
     |
     */
-    'logo' => '<b>Laravel</b> admin',
+    'logo' => '<b>HeyCommunity</b> admin',
 
     /*
     |--------------------------------------------------------------------------
@@ -34,7 +34,7 @@ return [
     | '<img src="http://logo-url" alt="Admin logo">'.
     |
     */
-    'logo-mini' => '<b>La</b>',
+    'logo-mini' => '<b>HC</b>',
 
     /*
     |--------------------------------------------------------------------------
