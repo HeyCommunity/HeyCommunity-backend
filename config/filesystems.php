0e2ee0f3385233d37acf0173d59accc6be77e065
SupeRodv2 <rod@protobia.tech>
2021-04-28 23:35:46 +0800

    admin disk

--- config/filesystems.php
+++ config/filesystems.php
@@ -35,6 +35,11 @@ return [
             'root' => storage_path('app'),
         ],
 
+        'admin' => [
+            'driver' => 'local',
+            'root' => storage_path('app'),
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
