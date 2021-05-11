4fd019bcf9a66823abcf4e112551a8bd78dae87c
SupeRodv2 <rod@protobia.tech>
2021-05-12 03:39:13 +0800

    the notice content

--- app/Http/Resources/NoticeResource.php
+++ app/Http/Resources/NoticeResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Notice;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class NoticeResource extends JsonResource
@@ -17,6 +18,7 @@ class NoticeResource extends JsonResource
         $data = parent::toArray($request);
 
         $data['sender']     =   $this->sender;
+        $data['type']       =   Notice::$types[$this->type];
         $data['content']    =   $this->getNoticeContent();
         $data['created_at_for_humans'] = $this->created_at_for_humans;
 
@@ -28,14 +30,23 @@ class NoticeResource extends JsonResource
      */
     protected function getNoticeContent()
     {
-        if ($this->type === 'post_thumb_up') {
-            return $this->entity->entity->content;
+        switch ($this->type) {
+            case 'post_thumb_up':
+            case 'post_comment_thumb_up':
+                $content = $this->entity->entity->content;
+                break;
+            case 'post_comment':
+                $content = $this->entity->content;
+                break;
+            case 'replay_post_comment':
+                $content = $this->entity->content;
+                break;
+            default:
+                $content = '未知消息内容';
+                break;
         }
 
-        if ($this->type === 'post_comment') {
-            return $this->entity->content;
-        }
-
-        return null;
+        if (! $content) $content = '未知消息内容';
+        return $content;
     }
 }
