ff32bb6dc0df96c3da70a428ce2da7253786a3bd
SupeRodv2 <rod@protobia.tech>
2021-05-26 15:12:22 +0800

    the wxapp subscribe message config

--- app/Http/Controllers/Api/SystemController.php
+++ app/Http/Controllers/Api/SystemController.php
@@ -13,7 +13,18 @@ class SystemController extends Controller
     public function settings()
     {
         return [
-            'ugc_audit'     =>  config('system.ugc_audit'),
+            'data' => [
+                // 用户生产内容 审核
+                'ugc_audit'     =>  config('system.ugc_audit'),
+
+                // 微信订阅消息
+                'wxapp_subscribe_message'  =>  [
+                    'enable'                =>  config('system.wxapp.subscribe_message.enable'),
+                    'thumb_up_temp_id'      =>  config('system.wxapp.subscribe_message.thumb_up_temp_id'),
+                    'comment_temp_id'       =>  config('system.wxapp.subscribe_message.comment_temp_id'),
+                    'reply_temp_id'         =>  config('system.wxapp.subscribe_message.reply_temp_id'),
+                ],
+            ]
         ];
     }
 }
--- config/system.php
+++ config/system.php
@@ -7,6 +7,11 @@ return [
         'subscribe_message' =>  [
             'post_thumb_up'     =>  env('WXAPP_SUBSCRIBE_MESSAGE_POST_THUMB_UP'),
             'post_comment'      =>  env('WXAPP_SUBSCRIBE_MESSAGE_POST_COMMENT'),
+
+            'enable'                =>  env('WXAPP_SUBSCRIBE_MESSAGE_ENABLE', false),
+            'thumb_up_temp_id'      =>  env('WXAPP_SUBSCRIBE_MESSAGE_THUMB_UP_TEMP_ID'),
+            'comment_temp_id'       =>  env('WXAPP_SUBSCRIBE_MESSAGE_COMMENT_TEMP_ID'),
+            'reply_temp_id'         =>  env('WXAPP_SUBSCRIBE_MESSAGE_REPLY_TEMP_ID'),
         ]
     ]
 ];
