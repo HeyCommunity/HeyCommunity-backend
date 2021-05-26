7d94972d4041b5ce698c7b7dec74403ea2eb18e5
SupeRodv2 <rod@protobia.tech>
2021-05-26 23:54:17 +0800

    remove somethings

--- config/system.php
+++ config/system.php
@@ -5,9 +5,6 @@ return [
 
     'wxapp'     =>  [
         'subscribe_message' =>  [
-            'post_thumb_up'     =>  env('WXAPP_SUBSCRIBE_MESSAGE_POST_THUMB_UP'),
-            'post_comment'      =>  env('WXAPP_SUBSCRIBE_MESSAGE_POST_COMMENT'),
-
             'enable'                =>  env('WXAPP_SUBSCRIBE_MESSAGE_ENABLE', false),
             'thumb_up_temp_id'      =>  env('WXAPP_SUBSCRIBE_MESSAGE_THUMB_UP_TEMP_ID'),
             'comment_temp_id'       =>  env('WXAPP_SUBSCRIBE_MESSAGE_COMMENT_TEMP_ID'),
