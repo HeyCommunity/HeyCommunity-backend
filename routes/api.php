c6822fd1ad32c437bcb4229ceebf8cebf5aca2c9
SupeRodv2 <rod@protobia.tech>
2021-05-27 11:37:17 +0800

    api route optimization

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\SystemController;
+use App\Http\Controllers\Api\Post\PostController;
+use App\Http\Controllers\Api\Post\PostImageController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\NoticeController;
+use App\Http\Controllers\Api\Common\ThumbController;
+use App\Http\Controllers\Api\Common\CommentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,7 +22,7 @@ use App\Http\Controllers\Api\NoticeController;
 |
 */
 
-Route::get('/system/settings', [\App\Http\Controllers\API\SystemController::class, 'settings']);
+Route::get('/system/settings', [SystemController::class, 'settings']);
 
 Route::get('/users/login', [UserController::class, 'login']);
 Route::get('/users/ping', [UserController::class, 'ping']);
@@ -36,19 +41,19 @@ Route::middleware(['auth:sanctum'])->group(function () {
 });
 
 
-Route::get('posts', [\App\Http\Controllers\Api\Post\PostController::class, 'index']);
-Route::get('posts/{post}', [\App\Http\Controllers\Api\Post\PostController::class, 'show'])->where('post', '[0-9]+');
+Route::get('posts', [PostController::class, 'index']);
+Route::get('posts/{post}', [PostController::class, 'show'])->where('post', '[0-9]+');
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::post('posts', [\App\Http\Controllers\Api\Post\PostController::class, 'store']);
-    Route::post('posts/delete', [\App\Http\Controllers\Api\Post\PostController::class, 'destroy']);
-    Route::post('posts/hidden', [\App\Http\Controllers\Api\Post\PostController::class, 'hidden']);
+    Route::post('posts', [PostController::class, 'store']);
+    Route::post('posts/delete', [PostController::class, 'destroy']);
+    Route::post('posts/hidden', [PostController::class, 'hidden']);
 
-    Route::post('post-images', [\App\Http\Controllers\Api\Post\PostImageController::class, 'store']);
-    Route::post('post-video', [\App\Http\Controllers\Api\Post\PostController::class, 'uploadVideo']);
+    Route::post('post-images', [PostImageController::class, 'store']);
+    Route::post('post-video', [PostController::class, 'uploadVideo']);
 
-    Route::post('post-thumbs', [\App\Http\Controllers\Api\Common\ThumbController::class, 'postThumbHandler']);
-    Route::post('post-comment-thumbs', [\App\Http\Controllers\Api\Common\ThumbController::class, 'postCommentThumbHandler']);
+    Route::post('post-thumbs', [ThumbController::class, 'postThumbHandler']);
+    Route::post('post-comment-thumbs', [ThumbController::class, 'postCommentThumbHandler']);
 
-    Route::post('post-comments', [\App\Http\Controllers\Api\Common\CommentController::class, 'postCommentHandler']);
-    Route::post('post-comments/delete', [\App\Http\Controllers\Api\Common\CommentController::class, 'postCommentDestroyHandler']);
+    Route::post('post-comments', [CommentController::class, 'postCommentHandler']);
+    Route::post('post-comments/delete', [CommentController::class, 'postCommentDestroyHandler']);
 });
