10d0efca50527992fbe9f17053a9e993f76f6b83
SupeRodv2 <rod@protobia.tech>
2021-10-18 17:31:30 +0800

    feat(PostModule): 初始化 PostModule

---
+++ Modules/Post/Database/Seeders/PostDatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\Post\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class PostDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        // $this->call("OthersTableSeeder");
+    }
+}
