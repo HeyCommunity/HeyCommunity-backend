ef6399a524cdf83b55debfedee5a9d1b3d6229a7
SupeRodv2 <rod@protobia.tech>
2021-10-18 21:45:44 +0800

    optimize(Database): 数据库优化

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,13 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('wx_open_id')->nullable()->comment('Wechat Open ID');
             $table->string('wx_union_id')->nullable()->comment('Wechat Union ID');
+            $table->string('wx_open_id')->nullable()->comment('Wechat Open ID');
+            $table->boolean('is_admin')->default(0)->comment('Is Admin');
 
             $table->string('nickname')->nullable()->comment('Nickname');
             $table->string('realname')->nullable()->comment('Realname');
             $table->string('gender')->nullable()->comment('Gender');
             $table->string('bio')->nullable()->comment('Bio');
+            $table->string('intro', 500)->nullable()->comment('Intro');
             $table->string('avatar')->default('images/users/default-avatar.jpg')->comment('User Avatar');
             $table->string('cover')->default('images/users/default-cover.jpg')->comment('Profile Cover');
 
@@ -29,12 +31,15 @@ class CreateUsersTable extends Migration
             $table->string('email')->nullable()->comment('Email');
             $table->string('password')->nullable()->comment('Password');
 
-            $table->boolean('is_admin')->default(0)->comment('Is Admin');
-            $table->tinyInteger('ugc_safety_level')->default(0)->comment('UGC Safety Level');
+            $table->integer('unread_notice_num')->default(0)->comment('未读通知数');
+            $table->json('wx_user_info')->nullable()->comment('微信用户信息');
 
             $table->rememberToken();
+            $table->timestamp('last_active_at')->nullable()->comment('Last Active Time');
+
             $table->timestamps();
             $table->softDeletes();
+
         });
     }
 
