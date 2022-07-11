11c336e3577f1a16c6692473cfc72652ea605106
SupeRodv2 <supgeek.rod@gmail.com>
2022-07-11 15:33:41 +0800

    chore: pagination 和 errors

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        \Illuminate\Pagination\Paginator::useBootstrap();
+
         // 中国手机号码验证器
         Validator::extend('phone', function ($attribute, $value, $parameters, $validator) {
             return $validator->validateRegex($attribute, $value, ['/^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\d{8}$/']);
