936e33202c9412bc0c1b02c1c6bbe0fcf6bda6d5
SupeRodv2 <supgeek.rod@gmail.com>
2022-07-18 20:41:37 +0800

    web: 首页面

--- resources/views/web/home/index.blade.php
+++ resources/views/web/home/index.blade.php
@@ -16,19 +16,17 @@
       <div class="col-12 col-md-4">
         <div class="card">
           <div class="card-header">
-            <h4 class="card-header-title">动态信息</h4>
-            <span class="small text-muted">#{{ $post->id }}</span>
+            <h4 class="card-header-title">动态</h4>
+            <span class="small text-muted">POSTS</span>
           </div>
           <div class="card-body">
             <div class="list-group list-group-flush my-n3">
               <div class="list-group-item">
                 <div class="row align-items-center">
                   <div class="col">
-                    <h5 class="mb-0">发布人</h5>
+                    <h5 class="mb-0">总数量</h5>
                   </div>
-                  <div class="col-auto">
-                    <a class="small" href="{{ route('dashboard.users.show', $post->user) }}">{{ $post->user->nickname }}</a>
-                  </div>
+                  <div class="col-auto">{{ $posts->count() }}</div>
                 </div>
               </div>
             </div>
--- resources/views/web/layouts/default.blade.php
+++ resources/views/web/layouts/default.blade.php
@@ -17,27 +17,10 @@
 
   <!-- Theme CSS -->
   <link rel="stylesheet" href="{{ asset('assets/dashkit/css/theme.bundle.css') }}" id="stylesheetLight" />
-  <link rel="stylesheet" href="{{ asset('assets/dashkit/css/theme-dark.bundle.css') }}" id="stylesheetDark" />
-
-  <style>body { display: none; }</style>
-  <style rel="stylesheet">
-    img {
-      display: inline-block;
-    }
-  </style>
-
-  <!-- 设置页面布局 -->
-  <script>
-    localStorage.setItem('dashkitShowPopover', 'false');
-    localStorage.setItem('dashkitSidebarSize', 'base');
-    localStorage.setItem('dashkitNavColor', 'default');
-    localStorage.setItem('dashkitColorScheme', 'light');
-    localStorage.setItem('dashkitNavPosition', 'sidenav');    // combo | sidenav | topnav
-  </script>
 </head>
 <body>
   <!-- NAVIGATION -->
-  <nav class="navbar navbar-expand-lg">
+  <nav class="navbar navbar-expand-lg navbar-light">
     <div class="container">
       <button class="navbar-toggler me-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -45,202 +28,9 @@
 
       <!-- Form -->
       <form class="form-inline me-4 d-none d-lg-flex">
-        <div class="input-group input-group-rounded input-group-merge input-group-reverse" data-list='{"valueNames": ["name"]}'>
-
-          <!-- Input -->
-          <input type="search" class="form-control dropdown-toggle list-search" data-bs-toggle="dropdown" placeholder="Search" aria-label="Search">
-
-          <!-- Icon -->
-          <div class="input-group-text">
-            <i class="fe fe-search"></i>
-          </div>
-
-          <!-- Menu -->
-          <div class="dropdown-menu dropdown-menu-card">
-            <div class="card-body">
-
-              <!-- List group -->
-              <div class="list-group list-group-flush list-group-focus list my-n3">
-                <a class="list-group-item" href="team-overview.html">
-                  <div class="row align-items-center">
-                    <div class="col-auto">
-
-                      <!-- Avatar -->
-                      <div class="avatar">
-                        <img src="/assets/dashkit/img/avatars/teams/team-logo-1.jpg" alt="..." class="avatar-img rounded">
-                      </div>
-
-                    </div>
-                    <div class="col ms-n2">
-
-                      <!-- Title -->
-                      <h4 class="text-body text-focus mb-1 name">
-                        Airbnb
-                      </h4>
-
-                      <!-- Time -->
-                      <p class="small text-muted mb-0">
-                        <span class="fe fe-clock"></span> <time datetime="2018-05-24">Updated 2hr ago</time>
-                      </p>
-
-                    </div>
-                  </div> <!-- / .row -->
-                </a>
-                <a class="list-group-item" href="team-overview.html">
-                  <div class="row align-items-center">
-                    <div class="col-auto">
-
-                      <!-- Avatar -->
-                      <div class="avatar">
-                        <img src="/assets/dashkit/img/avatars/teams/team-logo-2.jpg" alt="..." class="avatar-img rounded">
-                      </div>
-
-                    </div>
-                    <div class="col ms-n2">
-
-                      <!-- Title -->
-                      <h4 class="text-body text-focus mb-1 name">
-                        Medium Corporation
-                      </h4>
-
-                      <!-- Time -->
-                      <p class="small text-muted mb-0">
-                        <span class="fe fe-clock"></span> <time datetime="2018-05-24">Updated 2hr ago</time>
-                      </p>
-
-                    </div>
-                  </div> <!-- / .row -->
-                </a>
-                <a class="list-group-item" href="project-overview.html">
-                  <div class="row align-items-center">
-                    <div class="col-auto">
-
-                      <!-- Avatar -->
-                      <div class="avatar avatar-4by3">
-                        <img src="/assets/dashkit/img/avatars/projects/project-1.jpg" alt="..." class="avatar-img rounded">
-                      </div>
-
-                    </div>
-                    <div class="col ms-n2">
-
-                      <!-- Title -->
-                      <h4 class="text-body text-focus mb-1 name">
-                        Homepage Redesign
-                      </h4>
-
-                      <!-- Time -->
-                      <p class="small text-muted mb-0">
-                        <span class="fe fe-clock"></span> <time datetime="2018-05-24">Updated 4hr ago</time>
-                      </p>
-
-                    </div>
-                  </div> <!-- / .row -->
-                </a>
-                <a class="list-group-item" href="project-overview.html">
-                  <div class="row align-items-center">
-                    <div class="col-auto">
-
-                      <!-- Avatar -->
-                      <div class="avatar avatar-4by3">
-                        <img src="/assets/dashkit/img/avatars/projects/project-2.jpg" alt="..." class="avatar-img rounded">
-                      </div>
-
-                    </div>
-                    <div class="col ms-n2">
-
-                      <!-- Title -->
-                      <h4 class="text-body text-focus mb-1 name">
-                        Travels & Time
-                      </h4>
-
-                      <!-- Time -->
-                      <p class="small text-muted mb-0">
-                        <span class="fe fe-clock"></span> <time datetime="2018-05-24">Updated 4hr ago</time>
-                      </p>
-
-                    </div>
-                  </div> <!-- / .row -->
-                </a>
-                <a class="list-group-item" href="project-overview.html">
-                  <div class="row align-items-center">
-                    <div class="col-auto">
-
-                      <!-- Avatar -->
-                      <div class="avatar avatar-4by3">
-                        <img src="/assets/dashkit/img/avatars/projects/project-3.jpg" alt="..." class="avatar-img rounded">
-                      </div>
-
-                    </div>
-                    <div class="col ms-n2">
-
-                      <!-- Title -->
-                      <h4 class="text-body text-focus mb-1 name">
-                        Safari Exploration
-                      </h4>
-
-                      <!-- Time -->
-                      <p class="small text-muted mb-0">
-                        <span class="fe fe-clock"></span> <time datetime="2018-05-24">Updated 4hr ago</time>
-                      </p>
-
-                    </div>
-                  </div> <!-- / .row -->
-                </a>
-                <a class="list-group-item" href="profile-posts.html">
-                  <div class="row align-items-center">
-                    <div class="col-auto">
-
-                      <!-- Avatar -->
-                      <div class="avatar">
-                        <img src="/assets/dashkit/img/avatars/profiles/avatar-1.jpg" alt="..." class="avatar-img rounded-circle">
-                      </div>
-
-                    </div>
-                    <div class="col ms-n2">
-
-                      <!-- Title -->
-                      <h4 class="text-body text-focus mb-1 name">
-                        Dianna Smiley
-                      </h4>
-
-                      <!-- Status -->
-                      <p class="text-body small mb-0">
-                        <span class="text-success">●</span> Online
-                      </p>
-
-                    </div>
-                  </div> <!-- / .row -->
-                </a>
-                <a class="list-group-item" href="profile-posts.html">
-                  <div class="row align-items-center">
-                    <div class="col-auto">
-
-                      <!-- Avatar -->
-                      <div class="avatar">
-                        <img src="/assets/dashkit/img/avatars/profiles/avatar-2.jpg" alt="..." class="avatar-img rounded-circle">
-                      </div>
-
-                    </div>
-                    <div class="col ms-n2">
-
-                      <!-- Title -->
-                      <h4 class="text-body text-focus mb-1 name">
-                        Ab Hadley
-                      </h4>
-
-                      <!-- Status -->
-                      <p class="text-body small mb-0">
-                        <span class="text-danger">●</span> Offline
-                      </p>
-
-                    </div>
-                  </div> <!-- / .row -->
-                </a>
-              </div>
-
-            </div>
-          </div> <!-- / .dropdown-menu -->
-
+        <div class="input-group input-group-rounded input-group-merge input-group-reverse">
+          <input type="search" class="form-control dropdown-toggle list-search" data-bs-toggle="dropdown" placeholder="Search not available">
+          <div class="input-group-text"><i class="fe fe-search"></i></div>
         </div>
       </form>
 
@@ -255,27 +45,28 @@
 
           <!-- Menu -->
           <div class="dropdown-menu dropdown-menu-end">
-            <a href="#" class="disabled dropdown-item">Profile</a>
-            <a href="#" class="disabled dropdown-item">Settings</a>
+            <a href="#" class="disabled dropdown-item">注册</a>
+            <a href="#" class="disabled dropdown-item">登录</a>
             <hr class="dropdown-divider">
-            <a href="#" class="disabled dropdown-item">Logout</a>
+            <a href="#" class="disabled dropdown-item">暂不可用</a>
           </div>
         </div>
       </div>
 
       <!-- Collapse -->
       <div class="collapse navbar-collapse me-lg-auto order-lg-first" id="navbar">
-        <a class="navbar-brand" href="#">HEY社区</a>
-
         <!-- Form -->
         <form class="mt-4 mb-3 d-md-none">
-          <input type="search" class="form-control form-control-rounded" placeholder="Search" aria-label="Search">
+          <input type="search" class="form-control form-control-rounded" placeholder="Search not available">
         </form>
 
         <!-- Navigation -->
+        <a class="navbar-brand me-3" href="#">HEY社区</a>
         <ul class="navbar-nav me-lg-auto">
-          <li class="nav-item"><a class="nav-link" href="{{ route('web.home') }}">首页</a></li>
           <li class="nav-item"><a class="nav-link" href="{{ route('web.home') }}">动态</a></li>
+          <li class="nav-item"><a class="nav-link" href="{{ route('web.home') }}">文章</a></li>
+          <li class="nav-item"><a class="nav-link" href="{{ route('web.home') }}">话题</a></li>
+          <li class="nav-item"><a class="nav-link" href="{{ route('web.home') }}">关于</a></li>
         </ul>
       </div>
     </div>
