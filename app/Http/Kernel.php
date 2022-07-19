ddfe7c7b0fc028ffcd1433a1a20f2cdf3800084b
SupeRodv2 <rod@protobia.tech>
2022-07-20 01:14:34 +0800

    debug: 调试 laravel-development action - 2

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -23,7 +23,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
 
-        \App\Http\Middleware\VisitorLogging::class,                 // 访客日志
+        // \App\Http\Middleware\VisitorLogging::class,                 // 访客日志
     ];
 
     /**
