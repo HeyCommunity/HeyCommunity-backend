ec794c00fba9815293eac987b6656c438f4a6fc9
SupeRodv2 <supgeek.rod@gmail.com>
2022-07-19 12:21:59 +0800

    fix: 修复 WEB 首页面

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::namespace('App\\Http\\Controllers\\Web')->group(function () {
     Route::get('/', function () {
-        return route('web.posts.index');
+        return redirect()->route('web.posts.index');
     })->name('web.home');
 
     Route::get('debug', 'HomeController@debug')->name('web.home.debug');
