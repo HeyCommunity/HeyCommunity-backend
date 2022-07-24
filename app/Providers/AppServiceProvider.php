a69f15f7d2dcdcf7ef100ed4aaf566452dcbafd9
SupeRodv2 <rod@protobia.tech>
2022-07-24 17:14:56 +0800

    chore: 优化 composer.json 和 AppProviders

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        if (config('telescope.enabled')) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+        }
     }
 
     /**
