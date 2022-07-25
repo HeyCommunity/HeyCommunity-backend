6c4f318ac3adca1756e00883cf536f02aef883f3
SupeRodv2 <rod@protobia.tech>
2022-07-25 16:49:47 +0800

    dashboard: 优化后台导航

--- resources/views/dashboard/layouts/_navbar-main.blade.php
+++ resources/views/dashboard/layouts/_navbar-main.blade.php
@@ -25,16 +25,16 @@
   </li>
 
   <li class="nav-item">
-    <a class="nav-link {{ request()->routeIs('dashboard.users.index') ? 'active' : null }}" href="{{ route('dashboard.users.index') }}"><i class="fe fe-users"></i> 用户</a>
+    <a class="nav-link {{ request()->routeIs('dashboard.users.index') ? 'active' : null }}" href="{{ route('dashboard.users.index') }}"><i class="fe fe-users"></i> 用户管理</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link {{ request()->routeIs('dashboard.posts.index') ? 'active' : null }}" href="{{ route('dashboard.posts.index') }}"><i class="fe fe-rss"></i> 动态</a>
+    <a class="nav-link {{ request()->routeIs('dashboard.posts.index') ? 'active' : null }}" href="{{ route('dashboard.posts.index') }}"><i class="fe fe-rss"></i> 动态管理</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link {{ request()->routeIs('dashboard.comments.index') ? 'active' : null }}" href="{{ route('dashboard.comments.index') }}"><i class="fe fe-message-square"></i> 评论</a>
+    <a class="nav-link {{ request()->routeIs('dashboard.comments.index') ? 'active' : null }}" href="{{ route('dashboard.comments.index') }}"><i class="fe fe-message-square"></i> 评论管理</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link {{ request()->routeIs('dashboard.thumbs.index') ? 'active' : null }}" href="{{ route('dashboard.thumbs.index') }}"><i class="fe fe-thumbs-up"></i> 点赞</a>
+    <a class="nav-link {{ request()->routeIs('dashboard.thumbs.index') ? 'active' : null }}" href="{{ route('dashboard.thumbs.index') }}"><i class="fe fe-thumbs-up"></i> 点赞管理</a>
   </li>
 </ul>
 
@@ -44,20 +44,15 @@
 <!-- 其他 -->
 <h6 class="navbar-heading">其他</h6>
 <ul class="navbar-nav mb-md-4">
-  <li class="nav-item d-md-none">
-    <a class="nav-link" data-bs-toggle="offcanvas" href="#sidebarOffcanvasActivity" aria-contrtols="sidebarOffcanvasActivity">
-      <span class="fe fe-bell"></span> Notifications
-    </a>
-  </li>
-
   <li class="nav-item">
-    <a class="nav-link d-none d-md-inline-block {{ request()->routeIs('dashboard.iframes.telescope') ? 'active' : '' }}"
+    <a class="nav-link {{ request()->routeIs('dashboard.iframes.telescope') ? 'active' : '' }}"
        href="{{ route('dashboard.iframes.telescope') }}"><i class="fe fe-cast"></i> Telescope</a>
-    <a class="nav-link d-inline-block d-md-none" target="_blank" href="{{ url('/dashboard/telescope') }}"><i class="fe fe-cast"></i> Telescope</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link d-none d-md-inline-block {{ request()->routeIs('dashboard.iframes.log-viewer') ? 'active' : '' }}"
+    <a class="nav-link {{ request()->routeIs('dashboard.iframes.log-viewer') ? 'active' : '' }}"
        href="{{ route('dashboard.iframes.log-viewer') }}"><i class="fe fe-alert-octagon"></i> LogViewer</a>
-    <a class="nav-link d-inline-block d-md-none" target="_blank" href="{{ url('/dashboard/log-viewer') }}"><i class="fe fe-alert-octagon"></i> LogViewer</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" target="_blank" href="{{ route('web.home') }}"><i class="fe fe-home"></i> {{ config('app.name') }}</a>
   </li>
 </ul>
