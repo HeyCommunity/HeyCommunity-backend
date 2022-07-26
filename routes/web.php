eae2a3172f4fdcc87e186fcddafaf99ce44122ff
SupeRodv2 <supgeek.rod@gmail.com>
2022-07-26 15:50:12 +0800

    optimize: 优化

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,10 @@ Route::namespace('App\\Http\\Controllers\\Web')->group(function () {
         return redirect()->route('web.posts.index');
     })->name('web.home');
 
-    Route::get('login', 'AuthController@login')->name('web.login');
-    Route::post('login', 'AuthController@loginHandler')->name('web.login-handler');
+    Route::middleware(['guest'])->group(function () {
+        Route::get('login', 'AuthController@login')->name('web.login');
+        Route::post('login', 'AuthController@loginHandler')->name('web.login-handler');
+    });
     Route::post('logout', 'AuthController@logoutHandler')->name('web.logout-handler');
 
     Route::get('debug', 'HomeController@debug')->name('web.home.debug');
