004a9ceee8c39be521051679e5ad2e14befc4916
SupeRodv2 <supgeek.rod@gmail.com>
2022-08-12 18:11:26 +0800

    dev: 仪表盘页面

--- app/Http/Controllers/Dashboard/HomeController.php
+++ app/Http/Controllers/Dashboard/HomeController.php
@@ -14,15 +14,48 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $totalUserNum = User::count();
-        $totalPostNum = Post::count();
-        $totalCommentNum = Comment::count();
-        $totalThumbUpNum = Thumb::where('type', 'thumb_up')->count();
+        return view('dashboard.home.index', [
+            'modelTrendData'            =>  $this->getModelTrendData(),
+            'mainChartConfigure'        =>  $this->getMainChartConfigure(),
+            'visitorLogChartConfigure'  =>  $this->getVisitorLogChartConfigure(),
+        ]);
+    }
 
+    protected function getModelTrendData()
+    {
+        $weekStartDate = today()->subDays(6);
+        $monthStartDate = today()->subMonth(1)->addDay();
+
+        return [
+            'user'  =>  [
+                'total'         =>  User::count(),
+                'week_num'      =>  User::whereDate('created_at', '>=', $weekStartDate)->count(),
+                'month_num'     =>  User::whereDate('created_at', '>=', $monthStartDate)->count(),
+            ],
+            'post'  =>  [
+                'total'         =>  Post::count(),
+                'week_num'      =>  Post::whereDate('created_at', '>=', $weekStartDate)->count(),
+                'month_num'     =>  Post::whereDate('created_at', '>=', $monthStartDate)->count(),
+            ],
+            'comment'  =>  [
+                'total'         =>  Comment::count(),
+                'week_num'      =>  Comment::whereDate('created_at', '>=', $weekStartDate)->count(),
+                'month_num'     =>  Comment::whereDate('created_at', '>=', $monthStartDate)->count(),
+            ],
+            'thumb_up'  =>  [
+                'total'         =>  Thumb::where('type', 'thumb_up')->count(),
+                'week_num'      =>  Thumb::where('type', 'thumb_up')->whereDate('created_at', '>=', $weekStartDate)->count(),
+                'month_num'     =>  Thumb::where('type', 'thumb_up')->whereDate('created_at', '>=', $monthStartDate)->count(),
+            ],
+        ];
+    }
+
+    protected function getMainChartConfigure()
+    {
         $startDate = now()->subDays(26);
         $endDate = now();
 
-        $mainLineChartConfigure = AnalyticsBase::makeLineChartConfigure($startDate, $endDate, [
+        return AnalyticsBase::makeLineChartConfigure($startDate, $endDate, [
             ['name' => '用户增长', 'class' => User::class, 'color' => '#2c7be5'],
             ['name' => '用户活跃', 'class' => VisitorLog::class, 'color' => '#2a9d8f', 'count_column' => 'DISTINCT user_id'],
             ['name' => '访客请求', 'class' => VisitorLog::class, 'hidden' => true],
@@ -30,13 +63,38 @@ class HomeController extends Controller
             ['name' => '点赞数', 'class' => Thumb::class, 'color' => '#6e84a3', 'hidden' => true],
             ['name' => '评论数', 'class' => Comment::class, 'color' => '#39afd1', 'hidden' => true],
         ]);
+    }
 
-        return view('dashboard.home.index', compact(
-            'totalUserNum',
-            'totalPostNum',
-            'totalCommentNum',
-            'totalThumbUpNum',
-            'mainLineChartConfigure',
-        ));
+    protected function getVisitorLogChartConfigure()
+    {
+        $startDate = now()->subMonth()->subDay();
+
+        $visitorLogTotal = VisitorLog::whereDate('created_at', '>=', $startDate)->count();
+        $wxappVisitorLogTotal = VisitorLog::whereDate('created_at', '>=', $startDate)->where('request_path', 'like', '/api/%')->count();
+        $webVisitorLogTotal = VisitorLog::whereDate('created_at', '>=', $startDate)->where('route_name', 'like', 'web.%')->count();
+        $otherVisitorLogTotal = $visitorLogTotal - $wxappVisitorLogTotal - $webVisitorLogTotal;
+
+        $userVisitorLogTotal = VisitorLog::whereNotNull('user_id')->whereDate('created_at', '>=', $startDate)->count();
+        $userWxappVisitorLogTotal = VisitorLog::whereNotNull('user_id')->whereDate('created_at', '>=', $startDate)->where('request_path', 'like', '/api/%')->count();
+        $userWebVisitorLogTotal = VisitorLog::whereNotNull('user_id')->whereDate('created_at', '>=', $startDate)->where('route_name', 'like', 'web.%')->count();
+        $userOtherVisitorLogTotal = $userVisitorLogTotal - $userWxappVisitorLogTotal - $userWebVisitorLogTotal;
+
+        return [
+            'type'  =>  'doughnut',
+            'data'  =>  [
+                'labels'    =>  ['小程序', '网站', '其他'],
+                'datasets'  =>  [
+                    [
+                        'data'              =>  [$wxappVisitorLogTotal, $webVisitorLogTotal, $otherVisitorLogTotal],
+                        'backgroundColor'   =>  ['#2C7BE5', '#A6C5F7', '#D2DDEC'],
+                    ],
+                    [
+                        'data'              =>  [$userWxappVisitorLogTotal, $userWebVisitorLogTotal, $userOtherVisitorLogTotal],
+                        'backgroundColor'   =>  ['#2C7BE5', '#A6C5F7', '#D2DDEC'],
+                        'hidden'            =>  'true'
+                    ],
+                ],
+            ],
+        ];
     }
 }
--- resources/views/dashboard/home/index.blade.php
+++ resources/views/dashboard/home/index.blade.php
@@ -13,25 +13,18 @@
           </div>
           <div class="col-auto">
             <ul class="nav nav-tabs header-tabs">
-              <li class="nav-item">
-                <a href="{{ route('dashboard.analytics.increases') }}" class="nav-link text-center">
-                  <h6 class="header-pretitle text-secondary">&nbsp;</h6>
-                  <h3 class="text-white mb-0">数据增长明细</h3>
-                </a>
-              </li>
-
-              <li class="nav-item" onclick="showMainLineChartDatasets([0, 1])">
+              <li class="nav-item" onclick="showMainChartDatasets([0, 1])">
                 <a href="#" class="nav-link text-center active" data-bs-toggle="tab">
                   <h6 class="header-pretitle text-secondary">
-                    +{{ collect($mainLineChartConfigure['data']['datasets'][0]['data'])->sum() }}
-                    / {{ collect($mainLineChartConfigure['data']['datasets'][1]['data'])->sum() }}
+                    +{{ collect($mainChartConfigure['data']['datasets'][0]['data'])->sum() }}
+                    / {{ collect($mainChartConfigure['data']['datasets'][1]['data'])->sum() }}
                   </h6>
                   <h3 class="text-white mb-0">用户趋势</h3>
                 </a>
               </li>
-              <li class="nav-item" onclick="showMainLineChartDatasets([2])">
+              <li class="nav-item" onclick="showMainChartDatasets([2])">
                 <a href="#" class="nav-link text-center" data-bs-toggle="tab">
-                  @if (($visitorLogNum = collect($mainLineChartConfigure['data']['datasets'][2]['data'])->sum()) < 1000)
+                  @if (($visitorLogNum = collect($mainChartConfigure['data']['datasets'][2]['data'])->sum()) < 1000)
                     <h6 class="header-pretitle text-secondary">{{ $visitorLogNum }}</h6>
                   @else
                     <h6 class="header-pretitle text-secondary">{{ round($visitorLogNum / 1000, 1) }}k</h6>
@@ -39,17 +32,17 @@
                   <h3 class="text-white mb-0">请求数</h3>
                 </a>
               </li>
-              <li class="nav-item" onclick="showMainLineChartDatasets([3])">
+              <li class="nav-item" onclick="showMainChartDatasets([3])">
                 <a href="#" class="nav-link text-center" data-bs-toggle="tab">
-                  <h6 class="header-pretitle text-secondary">{{ collect($mainLineChartConfigure['data']['datasets'][3]['data'])->sum() }}</h6>
+                  <h6 class="header-pretitle text-secondary">{{ collect($mainChartConfigure['data']['datasets'][3]['data'])->sum() }}</h6>
                   <h3 class="text-white mb-0">动态</h3>
                 </a>
               </li>
-              <li class="nav-item" onclick="showMainLineChartDatasets([4, 5])">
+              <li class="nav-item" onclick="showMainChartDatasets([4, 5])">
                 <a href="#" class="nav-link text-center" data-bs-toggle="tab">
                   <h6 class="header-pretitle text-secondary">
-                    {{ collect($mainLineChartConfigure['data']['datasets'][4]['data'])->sum() }}
-                    / {{ collect($mainLineChartConfigure['data']['datasets'][5]['data'])->sum() }}
+                    {{ collect($mainChartConfigure['data']['datasets'][4]['data'])->sum() }}
+                    / {{ collect($mainChartConfigure['data']['datasets'][5]['data'])->sum() }}
                   </h6>
                   <h3 class="text-white mb-0">点赞/评论</h3>
                 </a>
@@ -61,24 +54,24 @@
 
       <div class="header-footer">
         <div class="chart">
-          <canvas id="mainLineChart" class="chart-canvas"></canvas>
+          <canvas id="mainChart" class="chart-canvas"></canvas>
 
           @section('pageScript')
             <script>
               document.addEventListener('DOMContentLoaded', function () {
-                new Chart(document.getElementById('mainLineChart'), {!! json_encode($mainLineChartConfigure) !!});
+                new Chart(document.getElementById('mainChart'), {!! json_encode($mainChartConfigure) !!});
               });
 
-              function showMainLineChartDatasets(datasets) {
-                let mainLineChart = Chart.getChart(document.getElementById('mainLineChart'));
+              function showMainChartDatasets(datasets) {
+                let mainChart = Chart.getChart(document.getElementById('mainChart'));
 
-                mainLineChart.data.datasets.forEach(function(dataset, index) {
+                mainChart.data.datasets.forEach(function(dataset, index) {
                   dataset.hidden = true;
 
                   if (datasets.includes(index)) dataset.hidden = false;
                 });
 
-                mainLineChart.update();
+                mainChart.update();
               }
             </script>
           @append
@@ -110,12 +103,12 @@
       <div class="col-12 col-xl-4">
         <div class="card">
           <div class="card-header">
-            <h4 class="card-header-title">访客请求</h4>
+            <h4 class="card-header-title">访客请求 <small class="text-muted ms-1">近 1 个月</small></h4>
             <ul class="nav nav-tabs nav-tabs-sm card-header-tabs">
-              <li class="nav-item" data-toggle="chart" data-target="#trafficChart" data-trigger="click" data-action="toggle" data-dataset="0">
+              <li class="nav-item" onclick="showChartDatasets('#visitorLogChart', [0])">
                 <a href="#" class="nav-link active" data-bs-toggle="tab">全部</a>
               </li>
-              <li class="nav-item" data-toggle="chart" data-target="#trafficChart" data-trigger="click" data-action="toggle" data-dataset="1">
+              <li class="nav-item" onclick="showChartDatasets('#visitorLogChart', [1])">
                 <a href="#" class="nav-link" data-bs-toggle="tab">仅用户</a>
               </li>
             </ul>
@@ -123,9 +116,29 @@
 
           <div class="card-body">
             <div class="chart chart-appended">
-              <canvas id="trafficChart" class="chart-canvas" data-toggle="legend" data-target="#trafficChartLegend"></canvas>
+              <canvas id="visitorLogChart" class="chart-canvas" data-toggle="legend" data-target="#visitorLogChartLegend"></canvas>
             </div>
-            <div id="trafficChartLegend" class="chart-legend"></div>
+            <div id="visitorLogChartLegend" class="chart-legend"></div>
+
+            @section('pageScript')
+              <script>
+                document.addEventListener('DOMContentLoaded', function () {
+                  new Chart(document.getElementById('visitorLogChart'), {!! json_encode($visitorLogChartConfigure) !!});
+                });
+
+                function showChartDatasets(selector, datasets) {
+                  let thisChart = Chart.getChart(document.getElementById(selector.substring(1)));
+
+                  thisChart.data.datasets.forEach(function(dataset, index) {
+                    dataset.hidden = true;
+
+                    if (datasets.includes(index)) dataset.hidden = false;
+                  });
+
+                  thisChart.update();
+                }
+              </script>
+            @append
           </div>
         </div>
       </div>
@@ -138,9 +151,9 @@
             <div class="row align-items-center gx-0">
               <div class="col">
                 <h6 class="text-uppercase text-muted mb-2">用户</h6>
-                <span class="h2 mb-0">{{ $totalUserNum }}</span>
+                <span class="h2 mb-0">{{ $modelTrendData['user']['total'] }}</span>
                 <span class="mt-n1 badge bg-light-soft text-secondary">
-                  +1 / +123
+                  +{{ $modelTrendData['user']['week_num'] }} / +{{ $modelTrendData['user']['month_num'] }}
                 </span>
               </div>
               <div class="col-auto"><span class="h2 fe fe-users text-muted mb-0"></span></div>
@@ -155,9 +168,9 @@
             <div class="row align-items-center gx-0">
               <div class="col">
                 <h6 class="text-uppercase text-muted mb-2">动态</h6>
-                <span class="h2 mb-0">{{ $totalPostNum }}</span>
+                <span class="h2 mb-0">{{ $modelTrendData['post']['total'] }}</span>
                 <span class="mt-n1 badge bg-light-soft text-secondary">
-                  +1 / +123
+                  +{{ $modelTrendData['post']['week_num'] }} / +{{ $modelTrendData['post']['month_num'] }}
                 </span>
               </div>
               <div class="col-auto"><span class="h2 fe fe-rss text-muted mb-0"></span></div>
@@ -172,9 +185,9 @@
             <div class="row align-items-center gx-0">
               <div class="col">
                 <h6 class="text-uppercase text-muted mb-2">点赞</h6>
-                <span class="h2 mb-0">{{ $totalThumbUpNum }}</span>
+                <span class="h2 mb-0">{{ $modelTrendData['thumb_up']['total'] }}</span>
                 <span class="mt-n1 badge bg-light-soft text-secondary">
-                  +1 / +123
+                  +{{ $modelTrendData['thumb_up']['week_num'] }} / +{{ $modelTrendData['thumb_up']['month_num'] }}
                 </span>
               </div>
               <div class="col-auto"><span class="h2 fe fe-thumbs-up text-muted mb-0"></span></div>
@@ -189,9 +202,9 @@
             <div class="row align-items-center gx-0">
               <div class="col">
                 <h6 class="text-uppercase text-muted mb-2">评论</h6>
-                <span class="h2 mb-0">{{ $totalCommentNum }}</span>
+                <span class="h2 mb-0">{{ $modelTrendData['comment']['total'] }}</span>
                 <span class="mt-n1 badge bg-light-soft text-secondary">
-                  +1 / +123
+                  +{{ $modelTrendData['comment']['week_num'] }} / +{{ $modelTrendData['comment']['month_num'] }}
                 </span>
               </div>
               <div class="col-auto"><span class="h2 fe fe-message-square text-muted mb-0"></span></div>
@@ -206,7 +219,7 @@
         <div class="card">
           <div class="card-body">
             <div class="d-grid">
-              <a class="btn btn-light" href="{{ route('dashboard.analytics.increases') }}">查看数据增长明细</a>
+              <a class="btn btn-light" target="_blank" href="{{ route('dashboard.analytics.increases') }}">查看数据增长明细</a>
             </div>
           </div>
         </div>
