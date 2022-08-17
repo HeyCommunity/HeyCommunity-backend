57e5cb6e1dd147bd20121bdf00eb4dea73dba855
SupeRodv2 <supgeek.rod@gmail.com>
2022-08-17 18:56:16 +0800

    dev: 优化路由

--- Modules/Activity/Routes/api.php
+++ Modules/Activity/Routes/api.php
@@ -1,10 +1,10 @@
 <?php
 
 Route::prefix('api/activities')->group(function () {
-    Route::get('/', 'ActivityController@index');
-    Route::get('{activity}', 'ActivityController@show')->where('activity', '[0-9]+');
+    Route::get('/', 'ActivityController@index')->name('api.activities.index');
+    Route::get('{activity}', 'ActivityController@show')->name('api.activities.show')->where('activity', '[0-9]+');
 
     Route::middleware(['auth:sanctum'])->group(function () {
-        Route::post('{activity}/register', 'ActivityController@register')->where('activity', '[0-9]+');
+        Route::post('{activity}/register', 'ActivityController@register')->name('api.activities.register')->where('activity', '[0-9]+');
     });
 });
--- Modules/Post/Routes/api.php
+++ Modules/Post/Routes/api.php
@@ -3,16 +3,16 @@
 use Modules\Post\Http\Controllers\Api\PostController;
 
 Route::prefix('api/posts')->group(function () {
-    Route::get('/', [PostController::class, 'index']);
-    Route::get('user-posts', [PostController::class, 'userPosts']);
-    Route::get('{post}', [PostController::class, 'show'])->where('post', '[0-9]+');
+    Route::get('/', [PostController::class, 'index'])->name('api.posts.index');
+    Route::get('{post}', [PostController::class, 'show'])->name('api.posts.show')->where('post', '[0-9]+');
+    Route::get('user-posts', [PostController::class, 'userPosts'])->name('api.posts.user-posts');
 
     Route::middleware(['auth:sanctum'])->group(function () {
-        Route::post('/', [PostController::class, 'store']);
-        Route::post('delete', [PostController::class, 'destroy']);
-        Route::post('hidden', [PostController::class, 'hidden']);
+        Route::post('/', [PostController::class, 'store'])->name('api.posts.store');
+        Route::post('delete', [PostController::class, 'destroy'])->name('api.posts.delete');
+        Route::post('hidden', [PostController::class, 'hidden'])->name('api.posts.hidden');
 
-        Route::post('upload-image', [PostController::class, 'uploadImage']);
-        Route::post('upload-video', [PostController::class, 'uploadVideo']);
+        Route::post('upload-image', [PostController::class, 'uploadImage'])->name('api.posts.upload-image');
+        Route::post('upload-video', [PostController::class, 'uploadVideo'])->name('api.posts.upload-video');
     });
 });
