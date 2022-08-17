963c84f987105a839e4577e0158b653027139ea6
SupeRodv2 <supgeek.rod@gmail.com>
2022-08-17 19:57:41 +0800

    dev: 优化路由

--- routes/api.php
+++ routes/api.php
@@ -10,17 +10,6 @@ use App\Http\Controllers\Api\NoticeController;
 use App\Http\Controllers\Api\CommentController;
 use App\Http\Controllers\Api\ThumbController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 ##
 ## User API
 Route::prefix('users')->group(function () {
