3235d63ce9b6bda8d8fc1edf729bc4aa425f6eb1
SupeRodv2 <supgeek.rod@gmail.com>
2022-09-05 17:45:28 +0800

    dev: 点赞和评论优化

--- resources/views/dashboard/comments/index.blade.php
+++ resources/views/dashboard/comments/index.blade.php
@@ -30,7 +30,6 @@
                   <th style="max-width:370px;">内容</th>
                   <th>点赞/评论</th>
                   <th>发布时间</th>
-                  <th>操作</th>
                 </tr>
               </thead>
               <tbody>
@@ -41,30 +40,34 @@
                       <a href="{{ route('dashboard.users.show', $comment->user) }}" class="avatar avatar-xs d-inline-block me-2">
                         <img src="{{ asset($comment->user->avatar) }}" alt="{{ $comment->user->app_id }}" class="avatar-img rounded-circle">
                       </a>
-                      <a href="{{ route('dashboard.users.show', $comment->user) }}">{{ $comment->user->nickname ?: 'NULL' }}</a>
+                      <a class="text-black" href="{{ route('dashboard.users.show', $comment->user) }}">{{ $comment->user->nickname ?: 'NULL' }}</a>
                     </td>
 
                     <!-- 目标用户 -->
                     <td>
-                      <a href="{{ route('dashboard.users.show', $comment->commentable->user) }}" class="avatar avatar-xs d-inline-block me-2">
-                        <img src="{{ asset($comment->commentable->user->avatar) }}" alt="{{ $comment->commentable->user->app_id }}" class="avatar-img rounded-circle">
-                      </a>
-                      <a href="{{ route('dashboard.users.show', $comment->commentable->user) }}">{{ $comment->commentable->user->nickname ?: 'NULL' }}</span>
+                      @if ($comment->commentable->user)
+                        <a href="{{ route('dashboard.users.show', $comment->commentable->user) }}" class="avatar avatar-xs d-inline-block me-2">
+                          <img src="{{ asset($comment->commentable->user->avatar) }}" alt="{{ $comment->commentable->user->app_id }}" class="avatar-img rounded-circle">
+                        </a>
+                        <a class="text-black" href="{{ route('dashboard.users.show', $comment->commentable->user) }}">{{ $comment->commentable->user->nickname ?: 'NULL' }}</span>
+                      @else
+                        <span class="text-muted">-</span>
+                      @endif
                     </td>
 
                     <!-- 目标实体 -->
                     <td>
                       @if ($comment->entity_class === \Modules\Post\Entities\Post::class)
-                        <a class="d-block" href="{{ route('dashboard.posts.show', $comment->entity_id) }}">{{ $comment->entity_name }}(ID:{{ $comment->entity_id }})</a>
+                        <a class="d-block text-black" href="{{ route('dashboard.posts.show', $comment->entity_id) }}">{{ $comment->entity_name }}(ID:{{ $comment->entity_id }})</a>
                       @else
-                        <span class="d-block">{{ $comment->entity_name }}(ID:{{ $comment->entity_id }})</span>
+                        <span class="d-block text-black">{{ $comment->entity_name }}(ID:{{ $comment->entity_id }})</span>
                       @endif
 
                       @if ($comment->parent)
                         @if ($comment->parent->entity_class === \Modules\Post\Entities\Post::class)
-                          <a class="d-block mt-1" href="{{ route('dashboard.comments.show', $comment->parent->id) }}">评论(ID:{{ $comment->parent->id }})</a>
+                          <a class="d-block text-black mt-1" href="{{ route('dashboard.comments.show', $comment->parent->id) }}">评论(ID:{{ $comment->parent->id }})</a>
                         @else
-                          <a class="d-block mt-1" href="{{ route('dashboard.comments.show', $comment->parent->id) }}">评论(ID:{{ $comment->parent->id }})</a>
+                          <a class="d-block text-black mt-1" href="{{ route('dashboard.comments.show', $comment->parent->id) }}">评论(ID:{{ $comment->parent->id }})</a>
                         @endif
                       @endif
                     </td>
@@ -80,9 +83,6 @@
 
                     <td>{{ $comment->thumb_up_num }} / {{ $comment->comment_num }}</td>
                     <td><span data-bs-toggle="tooltip" title="{{ $comment->created_at->diffForHumans() }}">{{ $comment->created_at }}</span></td>
-                    <td>
-                      <a href="{{ route('dashboard.comments.show', $comment) }}" class="btn btn-sm btn-light d-inline-block">详情</a>
-                    </td>
                   </tr>
                 @endforeach
               </tbody>
--- resources/views/dashboard/thumbs/index.blade.php
+++ resources/views/dashboard/thumbs/index.blade.php
@@ -28,7 +28,6 @@
                   <th>目标用户</th>
                   <th>目标实体</th>
                   <th>时间</th>
-                  <th>操作</th>
                 </tr>
               </thead>
               <tbody>
@@ -39,33 +38,35 @@
                       <a href="{{ route('dashboard.users.show', $thumb->user) }}" class="avatar avatar-xs d-inline-block me-2">
                         <img src="{{ asset($thumb->user->avatar) }}" alt="{{ $thumb->user->id }}" class="avatar-img rounded-circle">
                       </a>
-                      <a href="{{ route('dashboard.users.show', $thumb->user) }}">{{ $thumb->user->nickname ?: 'NULL' }}</a>
+                      <a class="text-black" href="{{ route('dashboard.users.show', $thumb->user) }}">{{ $thumb->user->nickname ?: 'NULL' }}</a>
                     </td>
 
                     <td>
-                      <a href="{{ route('dashboard.users.show', $thumb->thumbable->user) }}" class="avatar avatar-xs d-inline-block me-2">
-                        <img src="{{ asset($thumb->thumbable->user->avatar) }}" alt="{{ $thumb->thumbable->user->app_id }}" class="avatar-img rounded-circle">
-                      </a>
-                      <a href="{{ route('dashboard.users.show', $thumb->thumbable->user) }}">{{ $thumb->thumbable->user->nickname ?: 'NULL' }}</a>
+                      @if ($thumb->thumbable->user)
+                        <a href="{{ route('dashboard.users.show', $thumb->thumbable->user) }}" class="avatar avatar-xs d-inline-block me-2">
+                          <img src="{{ asset($thumb->thumbable->user->avatar) }}" alt="{{ $thumb->thumbable->user->app_id }}" class="avatar-img rounded-circle">
+                        </a>
+                        <a class="text-black" href="{{ route('dashboard.users.show', $thumb->thumbable->user) }}">{{ $thumb->thumbable->user->nickname ?: 'NULL' }}</a>
+                      @else
+                        <span class="text-muted">-</span>
+                      @endif
                     </td>
 
                     <!-- 目标实体 -->
                     <td>
                       @if ($thumb->entity_class === \Modules\Post\Entities\Post::class)
-                        <a href="{{ route('dashboard.posts.show', $thumb->entity_id) }}">{{ $thumb->entity_name }}(ID:{{ $thumb->entity_id }})</a>
+                        <a class="text-black" href="{{ route('dashboard.posts.show', $thumb->entity_id) }}">{{ $thumb->entity_name }}(ID:{{ $thumb->entity_id }})</a>
                       @elseif ($thumb->entity_class === \App\Models\Common\Comment::class)
                         @if ($thumb->entity->entity_class === \Modules\Post\Entities\Post::class)
-                          <a class="d-block" href="{{ route('dashboard.posts.show', $thumb->entity->entity_id) }}">{{ $thumb->entity->entity_name }}(ID:{{ $thumb->entity->entity_id }})</a>
-                          <a href="{{ route('dashboard.comments.show', $thumb->entity->id) }}">{{ $thumb->entity_name }}(ID:{{ $thumb->entity_id }})</a>
+                          <a class="d-block text-black" href="{{ route('dashboard.posts.show', $thumb->entity->entity_id) }}">{{ $thumb->entity->entity_name }}(ID:{{ $thumb->entity->entity_id }})</a>
+                          <a class="text-black" href="{{ route('dashboard.comments.show', $thumb->entity->id) }}">{{ $thumb->entity_name }}(ID:{{ $thumb->entity_id }})</a>
                         @else
                           <span>{{ $thumb->entity_name }}(ID:{{ $thumb->entity_id }})</span>
                         @endif
-
                       @endif
                     </td>
 
                     <td><span data-bs-toggle="tooltip" title="{{ $thumb->created_at->diffForHumans() }}">{{ $thumb->created_at }}</span></td>
-                    <td>/</td>
                   </tr>
                 @endforeach
               </tbody>
