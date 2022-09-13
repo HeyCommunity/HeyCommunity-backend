7f0f5b851e85fbe2d1bd7c12f800363a67cfc9ff
SupeRodv2 <supgeek.rod@gmail.com>
2022-09-13 12:38:00 +0800

    dev: 优化 EntityTrial

--- app/Models/Common/EntityTrial.php
+++ app/Models/Common/EntityTrial.php
@@ -2,6 +2,10 @@
 
 namespace App\Models\Common;
 
+use Modules\Activity\Entities\Activity;
+use Modules\Article\Entities\Article;
+use Modules\Post\Entities\Post;
+
 trait EntityTrial
 {
     /**
@@ -10,10 +14,14 @@ trait EntityTrial
     public function getEntityNameAttribute()
     {
         switch ($this->entity_class) {
-            case 'Modules\Post\Entities\Post':
-                return '动态';
-            case 'App\Models\Common\Comment':
+            case Comment::class:
                 return '评论';
+            case Post::class:
+                return '动态';
+            case Article::class:
+                return '文章';
+            case Activity::class:
+                return '活动';
             default:
                 return '未知';
         }
