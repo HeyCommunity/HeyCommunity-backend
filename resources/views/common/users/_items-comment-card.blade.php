e13ca8ff5128f61db0c5cfaaee8e9e6cbc437877
SupeRodv2 <supgeek.rod@gmail.com>
2022-09-14 17:29:27 +0800

    dev: 网站页面优化

--- resources/views/common/users/_items-comment-card.blade.php
+++ resources/views/common/users/_items-comment-card.blade.php
@@ -2,18 +2,6 @@
   <div class="card"><div class="card-body">暂无评论</div></div>
 @else
   @foreach ($comments as $comment)
-    <div class="card">
-      <div class="card-body">{{ $comment->content }}</div>
-      <div class="card-footer py-2 bg-light-soft">
-        <div class="row">
-          <div class="col">
-            <span class="text-muted small">{{ $comment->entity_name }}({{ $comment->entity_id }})</span>
-          </div>
-          <div class="col-auto">
-            <span class="text-muted small">{{ $comment->created_at }}</span>
-          </div>
-        </div>
-      </div>
-    </div>
+    @include('common.users._item-comment-card', ['comment' => $comment])
   @endforeach
 @endif
--- resources/views/common/users/_items-up-thumb-card.blade.php
+++ resources/views/common/users/_items-up-thumb-card.blade.php
@@ -10,21 +10,21 @@
       <div class="card-body bg-light-soft">
         @switch($thumb->entity_class)
           @case(\App\Models\Common\Comment::class)
-          @include('common.users._item-comment-card', ['comment' => $thumb->entity])
-          @break
+            @include('common.users._item-comment-card', ['comment' => $thumb->entity])
+            @break
           @case(\Modules\Post\Entities\Post::class)
-          @include('post::common._item-post-card', ['post' => $thumb->entity])
-          @break
+            @include('post::common._item-post-card', ['post' => $thumb->entity])
+            @break
           @case(\Modules\Article\Entities\Article::class)
-          @include('article::common._item-article-card', ['article' => $thumb->entity])
-          @break
+            @include('article::common._item-article-card', ['article' => $thumb->entity])
+            @break
           @case(\Modules\Activity\Entities\Activity::class)
-          @include('activity::common._item-activity-card', ['activity' => $thumb->entity])
-          @break
+            @include('activity::common._item-activity-card', ['activity' => $thumb->entity])
+            @break
           @default
-          <div class="card">
-            <div class="card-body">coming soon ..</div>
-          </div>
+            <div class="card">
+              <div class="card-body">coming soon ..</div>
+            </div>
         @endswitch
       </div>
     </div>
