c50c21eab21640b20be83d9892d9ae364d822159
SupeRodv2 <supgeek.rod@gmail.com>
2022-09-14 19:32:43 +0800

    dev: 动态前后台优化

--- Modules/Post/Resources/views/web/show.blade.php
+++ Modules/Post/Resources/views/web/show.blade.php
@@ -4,13 +4,17 @@
 <div class="main-content">
   <div class="container mt-4">
     <div class="row">
-      <div class="col-12 col-md-8">
+      <div class="col-12 col-lg-8">
         @include('post::common._item-post-card', ['post' => $post, 'showComments' => false])
+
+        <div class="d-block d-lg-none">
+          @include('common._up-thumbs-card', ['thumbs' => $post->upThumbs])
+        </div>
 
         @include('common._comments-card', ['comments' => $post->comments])
       </div>
 
-      <div class="col-12 col-md-4">
+      <div class="col-12 col-lg-4 d-none d-lg-block">
         @include('post::common._post-info-card', ['post' => $post])
 
         @include('common._up-thumbs-card', ['thumbs' => $post->upThumbs])
